1ed42ea52e55386e53e2802c062ca25324b7f7c7
Scott Weinstein <scottwww@squidnet.com>
2014-04-06 14:59:37 -0400

    update gulp/package

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,24 @@
 'use strict';
-// Generated on 2014-03-02 using generator-gulp-webapp 0.0.3
+// Generated on 2014-03-31 using generator-gulp-webapp 0.0.4
 
 var gulp = require('gulp');
+var wiredep = require('wiredep').stream;
 
 // Load plugins
 var $ = require('gulp-load-plugins')();
 
+
+// Styles
+gulp.task('styles', function () {
+    return gulp.src('app/styles/main.scss')
+        .pipe($.rubySass({
+          style: 'expanded',
+          loadPath: ['app/bower_components']
+        }))
+        .pipe($.autoprefixer('last 1 version'))
+        .pipe(gulp.dest('app/styles'))
+        .pipe($.size());
+});
 
 // Scripts
 gulp.task('scripts', function () {
@@ -37,11 +50,11 @@ gulp.task('images', function () {
 
 // Clean
 gulp.task('clean', function () {
-    return gulp.src(['dist/scripts', 'dist/images'], {read: false}).pipe($.clean());
+    return gulp.src(['dist/styles', 'dist/scripts', 'dist/images'], {read: false}).pipe($.clean());
 });
 
 // Bundle
-gulp.task('bundle', ['scripts'], $.bundle('./app/*.html'));
+gulp.task('bundle', ['styles', 'scripts'], $.bundle('./app/*.html'));
 
 // Build
 gulp.task('build', ['html', 'bundle', 'images']);
@@ -58,20 +71,46 @@ gulp.task('connect', $.connect.server({
     livereload: true
 }));
 
+// Inject Bower components
+gulp.task('wiredep', function () {
+    gulp.src('app/styles/*.scss')
+        .pipe(wiredep({
+            directory: 'app/bower_components',
+            ignorePath: 'app/bower_components/'
+        }))
+        .pipe(gulp.dest('app/styles'));
+
+    gulp.src('app/*.html')
+        .pipe(wiredep({
+            directory: 'app/bower_components',
+            ignorePath: 'app/'
+        }))
+        .pipe(gulp.dest('app'));
+});
+
 // Watch
 gulp.task('watch', ['connect'], function () {
     // Watch for changes in `app` folder
     gulp.watch([
         'app/*.html',
-        
+        'app/styles/**/*.css',
         'app/scripts/**/*.js',
         'app/images/**/*'
-    ], $.connect.reload);
-    
+    ], function(event) {
+        return gulp.src(event.path)
+            .pipe($.connect.reload());
+    });
+
+    // Watch .scss files
+    gulp.watch('app/styles/**/*.scss', ['styles']);
+
 
     // Watch .js files
     gulp.watch('app/scripts/**/*.js', ['scripts']);
 
     // Watch image files
     gulp.watch('app/images/**/*', ['images']);
+
+    // Watch bower files
+    gulp.watch('app/bower_components/*', ['wiredep']);
 });
