cd07f3c3020e3daba3155e55dff25f4957c2083d
Scott Weinstein <scottwww@squidnet.com>
2014-04-06 15:06:18 -0400

    deploy fixes

--- deploy.sh
+++ deploy.sh
@@ -112,19 +112,19 @@ if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd - > /dev/null
 fi
 
-# # 3. Install bower packages
-# if [ -e "$DEPLOYMENT_SOURCE/bower.json" ]; then
-#   eval $NPM_CMD install bower
-#   exitWithMessageOnError "installing bower failed"
-#   ./node_modules/.bin/bower install
-#   exitWithMessageOnError "bower failed"
-# fi
+# 3. Install bower packages
+if [ -e "$DEPLOYMENT_SOURCE/bower.json" ]; then
+  eval $NPM_CMD install bower
+  exitWithMessageOnError "installing bower failed"
+  ./node_modules/.bin/bower install
+  exitWithMessageOnError "bower failed"
+fi
 
-# # 4. Run gulp
-# if [ -e "$DEPLOYMENT_SOURCE/gulpfile.js" ]; then
-#   ./node_modules/.bin/gulp
-#   exitWithMessageOnError "gulp failed"
-# fi
+# 4. Run gulp
+if [ -e "$DEPLOYMENT_SOURCE/gulpfile.js" ]; then
+  ./node_modules/.bin/gulp
+  exitWithMessageOnError "gulp failed"
+fi
 
 
 # 1. KuduSync
